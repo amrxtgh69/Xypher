e9f015682e1e38da8f8f8c620deea12bc7832466
amrxtgh <ghimireamrit619@gmail.com>
2025-11-10 02:12:23 +0545

    add schema in the main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod indexer;
+mod crawler;
+
+use std::path::Path;
+use tantivy::schema::*;
+use indexer::setup_index;
+use std::io::{self, Write};
+
+fn main() -> tantivy::Resut<()> {
+    let index_path = Path::new(./main.rs;
+
+    //build schema
+    let mut schema_builder = Schema::builder();
+    let url_field = schema_builder.add_text_field("url", STORED);
+    let content_field = schema_builder.add_text_field("content", TEXT | STORED);
+    let schema = schema_builder.build();
 }
