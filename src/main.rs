5cb211c934976fe2dd24ce5574b15978e3ff079d
amrxtgh <ghimireamrit619@gmail.com>
2025-11-10 02:25:02 +0545

    open or create index

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod crawler;
 
 use std::path::Path;
 use tantivy::schema::*;
+use tantivy::Index;
 use indexer::setup_index;
 use std::io::{self, Write};
 
@@ -14,4 +15,12 @@ fn main() -> tantivy::Resut<()> {
     let url_field = schema_builder.add_text_field("url", STORED);
     let content_field = schema_builder.add_text_field("content", TEXT | STORED);
     let schema = schema_builder.build();
+
+    let index = if index_path.exists() {
+        Index::open_in_dir(&index_path).unwrap()
+    } else {
+        Index::create_in_dir(&index_path, schema.clone()).unwrap()
+    };
+
+
 }
