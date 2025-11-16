f28674cc2ee854569532a446c03aa9affa37ba4c
amrxtgh <ghimireamrit619@gmail.com>
2025-11-16 23:27:41 +0545

    add WebDocument struct for web scraping data representation

---
+++ src/models.rs
@@ -0,0 +1,12 @@
+use serde::{Serialize, Deserialize};
+
+#[derive( Deserialize, Debug, Clone)]
+pub struct WebDocument {
+    pub url: String,
+    pub title: Option<String>,
+    pub text: String,
+    pub favicon: Option<String>,
+    pub links: Vec<String>,
+    pub images: Vec<String>,
+    pub videos: Vec<String>,
+}
