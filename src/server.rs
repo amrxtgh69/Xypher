f3e6aff34e2f87eedac0b33de05a5de6275a1f24
amrxtgh <ghimireamrit619@gmail.com>
2025-11-17 15:12:32 +0545

    add server module with crawl and search handlers

---
+++ src/server.rs
@@ -0,0 +1,36 @@
+use std::sync::Arc;
+use axum::{routing::{post, get}, Json, Router, extract::State};
+use tokio::sync::{Mutex};
+
+use crate::{crawler::crawl_url, indexer::Indexer};
+use crate::models::WebDocument;
+
+
+pub async fn start_server() {
+    let indexer = Arc::new(Mutex::new(Indexer::new()));
+    
+    let app = Router::new()
+        .route("/crawl", post(crawl_handler))
+        .route("/search", get(search_handler))
+        .with_state(indexer);
+
+    println!("server listening on 127.0.0.1/6969");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:6969").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn crawl_handler(State(indexer): State<Arc<Mutex<Indexer>>>, Json(payload): Json<Vec<String>>) -> Json<Vec<WebDocument>> {
+    let mut docs = Vec::new();
+    
+    for url in payload {
+        if let Ok(doc) = crawl_url(&url).await {
+            docs.push(doc.clone());
+            indexer.lock().await.add(doc).unwrap();
+        }
+    }
+    Json(docs)
+}
+
+async fn search_handler(State(_indexer): State<Arc<Mutex<Indexer>>>) -> Json<String> {
+    Json("Search logic to do".to_string())
+}
